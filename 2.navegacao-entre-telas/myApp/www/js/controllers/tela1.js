0241df7a8f6a9c908043a0bcf3795f7f9933d0a1
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-08-01 17:46:50 -0300

    arquivos de controller e view da tela1.js|tela1.html e tela2.js|tela2.html

---
+++ 2.navegacao-entre-telas/myApp/www/js/controllers/tela1.js
@@ -0,0 +1,8 @@
+angular.module('starter')
+.controller('Tela1', tela1);
+
+function tela1 ($scope){
+
+  $scope.dado = "TELA1";
+
+}
---
+++ 2.navegacao-entre-telas/myApp/www/js/controllers/tela2.js
@@ -0,0 +1,8 @@
+angular.module('starter')
+.controller('Tela2', tela2);
+
+function tela2 ($scope){
+
+  $scope.dado = 'TELA2';
+
+}
