40e676eca769403bdd50cfc1d458c754b187f8c8
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-08-01 17:46:00 -0300

    configurando arquivo de routes.js

---
+++ 2.navegacao-entre-telas/myApp/www/js/routes.js
@@ -0,0 +1,18 @@
+angular.module('starter')
+.config(routeConfig);
+
+function routeConfig($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise('/t1');
+
+  $stateProvider.state('T1', {
+    url: '/t1',
+    templateUrl: 'views/tela1.html',
+    controller: 'Tela1'
+  })
+  .state('T2', {
+    url: '/t2',
+    templateUrl: 'views/tela2.html',
+    controller: 'Tela2'
+  });
+}
