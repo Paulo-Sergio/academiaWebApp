1ce8b05099db06b2d0ff43027a7294270a90a11e
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-08-10 11:19:52 -0300

    funcionamento do GPS ng-cordova

--- 7.ng-cordova/NGCordova/www/js/controllerTest.js
+++ 7.ng-cordova/NGCordova/www/js/controllerTest.js
@@ -1,10 +1,16 @@
 angular.module('starter')
-.controller('controllerTest', function($scope, $cordovaToast, $cordovaFile){
+.controller('controllerTest', function($scope, $cordovaToast, $cordovaFile, $cordovaCamera, $cordovaGeolocation){
 
+  /**
+  ****** MOSTRANDO PLUGIN DO TOAST
+  */
   $scope.mostrarToast = function(){
     $cordovaToast.showLongBottom('Mostrando mensagem');
   };
 
+  /**
+  ****** MOSTRANDO PLUGIN DE ESCRITA E LEITURA DE DOCUMENTOS
+  */
   $scope.gravar = function(){
     $cordovaFile.writeFile(cordova.file.dataDirectory, "DATA.txt", "Data: " + new Date(), true)
       .then(function(success){
@@ -28,5 +34,44 @@ angular.module('starter')
       });
   };
 
+
+  /**
+  ****** MOSTRANDO PLUGIN DE CAMERA
+  */
+  $scope.tirarFoto = function(){
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      saveToPhotoAlbum: false,
+	    correctOrientation:true
+    };
+
+    $cordovaCamera.getPicture(options).then(
+      function(imageData){
+        $scope.imagem = "data:image/jpeg;base64," + imageData;
+      },
+      function(err){
+        //error
+      });
+  };
+
+  /**
+  ****** MOSTRANDO GEOLOCALIZACAO (GPS)
+  */
+  $scope.getLocalizacao = function(){
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
+        $scope.localizacao = position.coords.latitude + ' - ' + position.coords.longitude;
+      }, function(err) {
+        // error
+      });
+  };
 
 });
